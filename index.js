770cda3af1dbb2cac3f36734b7f73dbf17b2f985
Neil <zooming.in@hotmail.com>
2021-04-26 22:41:42 +1000

    updates

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const name = ["Lisa", "Kaitlin", "Jan"];
+const event = "surprise";
+
+function writeCards (name, event) {
+    for (let i = 0; i < name.length; i++) {
+    console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    };
+}
+writeCards(name, event);
+
+
+function countDown(){
+ let number = 4;
+ while (number >= 0) {
+     console.log (number--)
+ }
+}
+
+countDown();
+
